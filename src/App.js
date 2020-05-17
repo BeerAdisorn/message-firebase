f86d823b11393214e0e5f4601fd72ccb201df421
adisornnakyim2@gmail.com <beer0649894255>
2020-05-18 03:47:32 +0700

    git init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
+import MessageList from './components/MessageList';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    var firebaseConfig = {
+      apiKey: "AIzaSyBI_f-n7B6V_OMoSxUG7jzWfAm45FyQeAA",
+      authDomain: "message-firebase-5e80f.firebaseapp.com",
+      databaseURL: "https://message-firebase-5e80f.firebaseio.com",
+      projectId: "message-firebase-5e80f",
+      storageBucket: "message-firebase-5e80f.appspot.com",
+      messagingSenderId: "895707823632",
+      appId: "1:895707823632:web:22ace12df0200410bbd495",
+      measurementId: "G-XY5CHC4L3L"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <Header title="Simple Firebase App" />
+        <div className="columns">
+          <div className="column is-3"></div>
+          <div className="column is-6">
+            <MessageList db={firebase} />
+          </div>
+        </div>
+
+      </div>
+    )
+  }
 }
 
 export default App;
---
+++ src/components/MessageBox.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class MessageBox extends Component {
+    render() {
+        return (
+            <form>
+                <textarea className="textarea" placeholder="Type a message" cols="100">
+
+                </textarea>
+            </form>
+        )
+    }
+}
+
+export default MessageBox; 
